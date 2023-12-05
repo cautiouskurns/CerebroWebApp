0421710184e71b58b548923f0b7c024b16efd04d
cautiouskurns <114108720+cautiouskurns@users.noreply.github.com>
2023-12-05 23:10:13 +0000

    Styling, kind of

--- app/main.py
+++ app/main.py
@@ -1,10 +1,33 @@
 
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
+import pandas as pd
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Cerebro!'
+def index():
+    return render_template('upload.html')
+
+ALLOWED_EXTENSIONS = {'csv'}
+
+# Global variable to store the data
+uploaded_data = None
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        if file.filename == '':
+            return redirect(request.url)
+        if file and file.filename.endswith('.csv'):
+            df = pd.read_csv(file, encoding='ISO-8859-1')
+            return df.to_html(header="true", table_id="table")
+        return redirect(request.url)
 
 if __name__ == '__main__':
     app.run(port=5001)
